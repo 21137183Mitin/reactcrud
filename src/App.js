7a4a13116824b820fcccfe32384ceb981016c76e
Anton Mitin <21137183@edu.riveria.fi>
2021-05-26 14:49:52 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <div className="container">
+      <h2>Data Base Tutorial</h2>
+    </div>
+  );
+}
+
+export default App;
